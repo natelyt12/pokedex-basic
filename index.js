edd419c0cf11ca5a5477c2b5816ce3b7b0859fd0
natelyt12 <nathanelytra2k7@gmail.com>
2023-11-15 22:38:35 +0700

    Upload Files

---
+++ index.js
@@ -0,0 +1,39 @@
+const input = document.getElementById("search");
+const charname = document.querySelector(".PokeName");
+const type = document.querySelector(".PokeType");
+const height = document.querySelector(".PokeHeight");
+const weight = document.querySelector(".PokeWeight");
+const bio = document.querySelector(".PokeBio");
+const img = document.querySelector(".PokeImg")
+
+
+function submit() {
+    var realinput = input.value.toLowerCase();
+    console.log(realinput)
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${realinput}`)
+
+    .then(response => response.json())
+
+    .then(data => {
+        console.log(data)
+        charname.innerHTML = data.name;
+        type.innerHTML = data.types[0].type.name;
+
+        height.innerHTML = data.height /= 10;
+        var kg = data.weight;
+        var lbs = kg /= 10;
+        weight.innerHTML = Math.round(lbs *= 2.20462);
+        
+        img.src = data.sprites.versions['generation-v']['black-white'].animated.front_default
+        bio.innerHTML = "Can't find description:("
+        // var getbio = data.species.url
+        // fetch(getbio)
+
+        // .then(response => response.json())
+
+        // .then (data => {
+        //     console.log(data)
+        // })
+    })
+}
